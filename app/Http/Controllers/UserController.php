31fcc5ba075986406fdf5b6cdcbcf193f2c8d17c
Guilherme Mazur <jjguilh@gmail.com>
2022-08-24 18:37:54 -0300

    feat: adding some models, migrations and controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function register(Request $request){
+        User::create($request->toArray());
+    }
 }
---
+++ database/migrations/2022_08_24_192702_create_time_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Constraint\Constraint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                    ->constrained()
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            $table->dateTime('arrivalTime');
+            $table->dateTime('exitTime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_logs');
+    }
+};
