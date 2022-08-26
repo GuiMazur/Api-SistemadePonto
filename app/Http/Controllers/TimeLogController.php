6bd3b29d944afa64ab01805a086992ade0b000ad
Guilherme Mazur <jjguilh@gmail.com>
2022-08-26 11:05:23 -0300

    feat: updating some functions

--- app/Http/Controllers/TimeLogController.php
+++ app/Http/Controllers/TimeLogController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\TimeLog;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TimeLogController extends Controller
 {
     public function timeRecorder(Request $request){
         $timeData = $request->toArray();
+        $registeredPassword = User::find($timeData["user_id"])->toArray()["password"];
+        if ($timeData["password"] == $registeredPassword){
         TimeLog::create($timeData);
+        return "Registro criado com sucesso.";
+        }
     }
 
 
